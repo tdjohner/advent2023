170a69e2f6890b7a851d176c6836d445f01407c7
Teagan <tdjohner@gmail.com>
2023-12-01 16:25:20 -0700

    Initial commit

---
+++ 01_23.py
@@ -0,0 +1,8 @@
+# Teagan Johner
+# Advent of Code 2023
+# Day 01
+# https://adventofcode.com/2023/day/1
+
+import urllib.request
+
+urllib.request.urlretrieve("https://adventofcode.com/2023/day/1/input")
